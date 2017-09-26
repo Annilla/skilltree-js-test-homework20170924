cd621edc07c61cc21ad964cc5ea82914ce4a8bbd
Anny Chang <AnnyChang@kingstono365.onmicrosoft.com>
2017-09-26 22:01:20 +0800

    base - UI ok

--- features/step_definitions/shoppingCart.step.js
+++ features/step_definitions/shoppingCart.step.js
@@ -6,48 +6,16 @@ defineSupportCode(function ({ Given, When, Then }) {
     var url = "http://localhost:9000"
     Given('進入購物車頁面', async function () {
         await this.driver.goto(url)
-            .viewport(1000, 600);
+            .viewport(1000, 600).wait('#num');
     });
 
-    When('按 + 按鈕', function () {
-        // Write code here that turns the phrase above into concrete actions
+    When('按 + 按鈕', async function () {
+        await this.driver.click('#add').click('#add');
     });
 
-    Then('數量顯示為 {string}', function (string) {
-        // Write code here that turns the phrase above into concrete actions
-        console.log(string);
+    Then('數量顯示為 {string}', async function (string) {
+        let num = await this.driver
+                    .evaluate(() => document.querySelector('#num').innerText);
+        console.log(num);
     });
-
-    // When('按 + 按鈕', async function () {
-    //     var price = await this.driver
-    //         .evaluate(() => document.querySelector('#price').innerText);
-
-    //     this.actual = +price;
-    // });
-
-
-    // Then('折扣後價格為 {string} 元', function (expected) {
-    //     this.actual.should.equal(+expected);
-    // });
 })
-
-// var { client } = require('nightwatch-cucumber')
-// var { defineSupportCode } = require('cucumber')
-
-// defineSupportCode(({ Given, Then, When }) => {
-//     var url = "http://localhost:9000"
-//     Given('進入購物車頁面', function () {
-//         return client
-//             .url(url)
-//             .waitForElementVisible('#num', 1000)
-//     });
-
-//     When('按 + 按鈕', function () {
-//         // Write code here that turns the phrase above into concrete actions
-//     });
-
-//     Then('數量顯示為 {string}', function (string) {
-//         // Write code here that turns the phrase above into concrete actions
-//         console.log(string);
-//     });
-// })
--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,20 @@
     <main>
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
-          <v-flex lg4>
+          <v-flex lg4 v-for="(book, index) in bookList" :key="index">
             <v-card class="cyan darken-4 white--text">
               <v-container fluid grid-list-lg>
                 <v-layout row>
                   <v-flex xs7>
                     <div>
-                      <div class="headline">第1集</div>
-                      <div>數量： 
-                        <span id="num">{{ number }}</span>
-                      </div>
+                      <div class="headline">{{ book.name }}</div>
+                      <div class="subheading">{{ book.price }} 元 / 本</div>
+                      
                     </div>
                   </v-flex>
                   <v-flex xs5>
                     <v-card-media
-                      src="/public/book1.jpg"
+                      :src="'/public/book'+(index+1)+'.jpg'"
                       height="125px"
                       contain
                     ></v-card-media>
@@ -28,92 +27,27 @@
                 </v-layout>
               </v-container>
               <v-card-actions class="white">
+                <div class="black--text">購買</div>
                 <v-spacer></v-spacer>
-                <v-btn icon @click="addNumber">
-                  <v-icon>add</v-icon>
-                </v-btn>
-                <v-btn icon>
+                <v-btn icon @click="removeBook(index)">
                   <v-icon>remove</v-icon>
+                </v-btn>
+                <div class="black--text">{{ book.number }}</div>
+                <v-btn icon @click="addBook(index)">
+                  <v-icon>add</v-icon>
                 </v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
-          <v-flex lg4>
-            <v-card class="cyan darken-4 white--text">
+          <v-flex xs12 sm6>
+            <v-card>
               <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
-                    <div>
-                      <div class="headline">第1集</div>
-                      <div>數量： 1</div>
-                    </div>
+                <v-layout>
+                  <v-flex xs6>
+                    總金額:
                   </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="/public/book1.jpg"
-                      height="125px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex><v-flex lg4>
-            <v-card class="cyan darken-4 white--text">
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
-                    <div>
-                      <div class="headline">第1集</div>
-                      <div>數量： 1</div>
-                    </div>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="/public/book1.jpg"
-                      height="125px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex><v-flex lg4>
-            <v-card class="cyan darken-4 white--text">
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
-                    <div>
-                      <div class="headline">第1集</div>
-                      <div>數量： 1</div>
-                    </div>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="/public/book1.jpg"
-                      height="125px"
-                      contain
-                    ></v-card-media>
-                  </v-flex>
-                </v-layout>
-              </v-container>
-            </v-card>
-          </v-flex><v-flex lg4>
-            <v-card class="cyan darken-4 white--text">
-              <v-container fluid grid-list-lg>
-                <v-layout row>
-                  <v-flex xs7>
-                    <div>
-                      <div class="headline">第1集</div>
-                      <div>數量： 1</div>
-                    </div>
-                  </v-flex>
-                  <v-flex xs5>
-                    <v-card-media
-                      src="/public/book1.jpg"
-                      height="125px"
-                      contain
-                    ></v-card-media>
+                  <v-flex xs6 class="text-xs-right">
+                    0
                   </v-flex>
                 </v-layout>
               </v-container>
@@ -129,12 +63,42 @@
   export default {
     data () {
       return {
-        number: 0
+        bookList: [
+          {
+            name: '第1集',
+            number: 0,
+            price: 100
+          },
+          {
+            name: '第2集',
+            number: 0,
+            price: 100
+          },
+          {
+            name: '第3集',
+            number: 0,
+            price: 100
+          },
+          {
+            name: '第4集',
+            number: 0,
+            price: 100
+          },
+          {
+            name: '第5集',
+            number: 0,
+            price: 100
+          }
+        ]
       }
     },
     methods: {
-      addNumber() {
-        this.number ++;
+      addBook(index) {
+        this.bookList[index].number ++;
+      },
+      removeBook(index) {
+        let num = this.bookList[index].number;
+        this.bookList[index].number = num <= 0 ? 0 : num-1;
       }
     }
   }
